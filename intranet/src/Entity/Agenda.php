dbfb0253fc186b4b7a7a3ab6309f515351b8012a
mapichec <marco.picheca@gmail.com>
2025-08-01 12:40:21 -0400

    relation OneToMany created; bin/console make:migrate and [200~bin/console doctrine:migrations:migrateto be done;

--- intranet/src/Entity/Agenda.php
+++ intranet/src/Entity/Agenda.php
@@ -5,6 +5,9 @@ namespace App\Entity;
 use App\Repository\AgendaRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Event;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: AgendaRepository::class)]
 class Agenda
@@ -17,6 +20,39 @@ class Agenda
     #[ORM\Column(type: Types::ARRAY, nullable: true)]
     private ?array $users = null;
 
+    #[ORM\OneToMany(mappedBy: 'agenda', targetEntity: Event::class, cascade: ['persist', 'remove'])]
+    private Collection $events;
+    
+    public function __construct()
+    {
+        $this->events = new ArrayCollection();
+    }
+    
+    public function getEvents(): Collection
+    {
+        return $this->events;
+    }
+    
+    public function addEvent(Event $event): static
+    {
+        if (!$this->events->contains($event)) {
+            $this->events[] = $event;
+            $event->setAgenda($this);
+        }
+    
+        return $this;
+    }
+    
+    public function removeEvent(Event $event): static
+    {
+        if ($this->events->removeElement($event)) {
+            if ($event->getAgenda() === $this) {
+                $event->setAgenda(null);
+            }
+        }
+    
+        return $this;
+    }
     public function getId(): ?int
     {
         return $this->id;
