eff2d44783f48452e1fb3237c8110b9500a46bd7
marcotani <marco.tani92@gmail.com>
2025-08-01 18:46:25 +0200

    Added creation of users and mailer. Using mailhog to test confirmation emails

--- intranet/src/Entity/User.php
+++ intranet/src/Entity/User.php
@@ -8,9 +8,11 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'This email is already registered.')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -24,10 +26,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 60)]
     private ?string $last_name = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(length: 100, unique: true)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 100)]
+    #[ORM\Column(length: 100, nullable: true)]
     private ?string $password = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
@@ -35,6 +37,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column(enumType: UserRole::class)]
     private ?UserRole $role = null;
+
+    // Properieta per la gestione della conferma dell'email
+
+    #[ORM\Column(length: 64, nullable: true, unique: true)]
+    private ?string $confirmationToken = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private bool $isActive = false;
 
     public function getId(): ?int
     {
@@ -49,7 +59,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setFirstName(string $first_name): static
     {
         $this->first_name = $first_name;
-
         return $this;
     }
 
@@ -61,7 +70,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setLastName(string $last_name): static
     {
         $this->last_name = $last_name;
-
         return $this;
     }
 
@@ -73,7 +81,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): static
     {
         $this->email = $email;
-
         return $this;
     }
 
@@ -82,10 +89,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this->password;
     }
 
-    public function setPassword(string $password): static
+    public function setPassword(?string $password): static
     {
         $this->password = $password;
-
         return $this;
     }
 
@@ -97,7 +103,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setCreated(\DateTime $created): static
     {
         $this->created = $created;
-
         return $this;
     }
 
@@ -109,11 +114,30 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRole(UserRole $role): static
     {
         $this->role = $role;
-
         return $this;
     }
 
-    // Implement UserInterface & PasswordAuthenticatedUserInterface methods
+    public function getConfirmationToken(): ?string
+    {
+        return $this->confirmationToken;
+    }
+
+    public function setConfirmationToken(?string $confirmationToken): static
+    {
+        $this->confirmationToken = $confirmationToken;
+        return $this;
+    }
+
+    public function getIsActive(): bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): static
+    {
+        $this->isActive = $isActive;
+        return $this;
+    }
 
     public function getUserIdentifier(): string
     {
@@ -122,12 +146,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function getRoles(): array
     {
-        // Assuming your UserRole enum values are strings like "ROLE_ADMIN"
         return $this->role ? [$this->role->value] : [];
     }
 
     public function eraseCredentials(): void
     {
-        // If you had any temporary sensitive data, clear it here.
+        // Clear temporary sensitive data if any
     }
 }
