d6b3eafa7de6de797e917674c1aa6844745af5ef
Marco Tani <mtani@student.42firenze.it>
2025-08-02 23:06:51 +0200

    Added buttons to homepage if you're already signed in to proceed to profile or logout. Added buttons to userpage to edit profile info or logout. Fixed a few issues with twig templates that threw errors instead of displaying certain pages and added a bit of white background for legibility. Modified database info back to what it was (sorry Marco) because the new one was giving me some issues

--- intranet/migrations/Version20250802195622.php
+++ intranet/migrations/Version20250802195622.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250802104937 extends AbstractMigration
+final class Version20250802195622 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -22,18 +22,22 @@ final class Version20250802104937 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE agenda (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, agenda_id INT NOT NULL, name VARCHAR(255) NOT NULL, max_participants INT NOT NULL, participants INT NOT NULL, description LONGTEXT NOT NULL, title VARCHAR(255) NOT NULL, date DATE NOT NULL, start_time TIME NOT NULL, end_time TIME NOT NULL, duration DOUBLE PRECISION NOT NULL, INDEX IDX_3BAE0AA7EA67784A (agenda_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, agenda_id INT DEFAULT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(60) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(100) DEFAULT NULL, created DATE NOT NULL, role VARCHAR(255) NOT NULL, confirmation_token VARCHAR(64) DEFAULT NULL, is_active TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D649C05FB297 (confirmation_token), INDEX IDX_8D93D649EA67784A (agenda_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE event_user (event_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_92589AE271F7E88B (event_id), INDEX IDX_92589AE2A76ED395 (user_id), PRIMARY KEY(event_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(60) NOT NULL, email VARCHAR(100) NOT NULL, password VARCHAR(100) DEFAULT NULL, created DATE NOT NULL, role VARCHAR(255) NOT NULL, confirmation_token VARCHAR(64) DEFAULT NULL, is_active TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), UNIQUE INDEX UNIQ_8D93D649C05FB297 (confirmation_token), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7EA67784A FOREIGN KEY (agenda_id) REFERENCES agenda (id)');
-        $this->addSql('ALTER TABLE `user` ADD CONSTRAINT FK_8D93D649EA67784A FOREIGN KEY (agenda_id) REFERENCES agenda (id)');
+        $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE271F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE event_user ADD CONSTRAINT FK_92589AE2A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA7EA67784A');
-        $this->addSql('ALTER TABLE `user` DROP FOREIGN KEY FK_8D93D649EA67784A');
+        $this->addSql('ALTER TABLE event_user DROP FOREIGN KEY FK_92589AE271F7E88B');
+        $this->addSql('ALTER TABLE event_user DROP FOREIGN KEY FK_92589AE2A76ED395');
         $this->addSql('DROP TABLE agenda');
         $this->addSql('DROP TABLE event');
+        $this->addSql('DROP TABLE event_user');
         $this->addSql('DROP TABLE `user`');
     }
 }
