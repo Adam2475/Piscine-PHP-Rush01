f57f4e570da00b8258747cb699e2c48226fedea6
stescaro99 <stescaro@student.42firenze.it>
2025-08-04 10:33:21 +0200

    chat

---
+++ intranet/migrations/Version20250804075804.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250804075804 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE chat_message ADD media_url LONGTEXT DEFAULT NULL, ADD media_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE chat_message DROP media_url, DROP media_name');
+    }
+}
